2e4dea0c91d76d9980e04b705d44ed2d92314782
Flobby41 <165112753+flobby41@users.noreply.github.com>
2024-10-24 08:48:41 +0200

    usernames are all converted to lowercase when typed into the login field

---
+++ config/database.js
@@ -0,0 +1,9 @@
+// set up database module
+
+// if LOCAL 
+// module.exports={'url':'localhost:27017/nodeblog'};
+
+// if on SERVER
+module.exports={
+	'url':'mongodb://heroku_vxw8c3zf:33dan8d95i5lief8drc0fm2qdu@ds047722.mongolab.com:47722/heroku_vxw8c3zf'
+};
